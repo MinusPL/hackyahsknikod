fde9b6ef5e2ff7c69d4660b7f8d93d1abbb45099
KVBAMusic <kubaokon8@gmail.com>
2022-11-19 15:48:10 +0100

    add ObjectBehaviour

---
+++ HP/Assets/Scripts/Objects/ObjectBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+    public virtual void Tick(float delta)
+    {
+        
+    }
+
+}
