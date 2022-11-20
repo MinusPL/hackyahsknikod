5a7a1e5a229649eded0720fe3432171437c67b4c
Minus <theminusoid@gmail.com>
2022-11-20 10:34:29 +0100

    PAAAAAAAIN

--- HP_HDRP/Assets/UI/Scripts/MainMenu.cs
+++ HP_HDRP/Assets/UI/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Scenes/dupa");
+        SceneManager.LoadScene("Scenes/GamePlay");
     }
     public void QuitGame()
     {
